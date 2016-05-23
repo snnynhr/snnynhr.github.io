2b65d8ad696880a1403e67d93d70b52c78e83d62
Sunny Nahar <sunnyn@cmu.edu>
2016-05-22 21:23:02 -0400

    Massive revamp of webpage: split sections into pages, add pdf school content, link previous content, update css, add images, update javascript, remove dummy pages

--- js/grayscale.js
+++ js/grayscale.js
@@ -6,38 +6,46 @@
 
 // jQuery to collapse the navbar on scroll
 
-scroll = false;
-changed = false;
-
 $(window).scroll(function() {
-    console.log($('.navbar').offset().top);
-    if ($('.navbar').offset().top > 50) {
-        $('.navbar-fixed-top').addClass('top-nav-collapse');
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
-        $('.navbar-fixed-top').removeClass('top-nav-collapse');
-    }
-
-    if ($('.navbar').offset().top >= 620) {
-        scroll = true;
-    }
-    else
-    {
-        changed = false;
-        scroll = false;
-    }
-    if (scroll && !changed)
-    {
-      changed = true;
-      a = document.getElementsByClassName('intro');
-      console.log(a);
-      var v = Math.floor(Math.random() * 24.0) + 1;
-      for (i = 0; i < a.length; i++)
-      {
-        a[i].style.setProperty('background', 'url(img/' + v + '.jpg) no-repeat bottom center scroll');
-        a[i].style.setProperty('background-size', 'cover');
-      }
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
+
+// scroll = false;
+// changed = false;
+
+// $(window).scroll(function() {
+//     console.log($('.navbar').offset().top);
+//     if ($('.navbar').offset().top > 50) {
+//         $('.navbar-fixed-top').addClass('top-nav-collapse');
+//     } else {
+//         $('.navbar-fixed-top').removeClass('top-nav-collapse');
+//     }
+
+//     if ($('.navbar').offset().top >= 620) {
+//         scroll = true;
+//     }
+//     else
+//     {
+//         changed = false;
+//         scroll = false;
+//     }
+//     if (scroll && !changed)
+//     {
+//       changed = true;
+//       a = document.getElementsByClassName('intro');
+//       console.log(a);
+//       var v = Math.floor(Math.random() * 24.0) + 1;
+//       for (i = 0; i < a.length; i++)
+//       {
+//         a[i].style.setProperty('background', 'url(img/' + v + '.jpg) no-repeat bottom center scroll');
+//         a[i].style.setProperty('background-size', 'cover');
+//       }
+//     }
+// });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
